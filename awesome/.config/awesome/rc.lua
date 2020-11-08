3c6d26eca85b6fbc9c2d787fbc73c3f2aa2f404d
SimplyNesh <neshnotion@gmail.com>
2020-11-08 21:31:22 +0800

    modulated autostart and errors.lua

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,37 +1,5 @@
--- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
-
--- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
-local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
--- Declarative object management
-local ruled = require("ruled")
-local menubar = require("menubar")
-local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-naughty.connect_signal("request::display_error", function(message, startup)
-    naughty.notification {
-        urgency = "critical",
-        title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
-        message = message
-    }
-end)
--- }}}
+-- Error handling
+local errors = require("errors")
 
 --  Variable definitions
 local variables = require("variables")
@@ -62,14 +30,9 @@ client.connect_signal("mouse::enter", function(c)
     c:activate { context = "mouse_enter", raise = false }
 end)
 
-
 --client.connect_signal("manage", function (c)
 --    c.shape = gears.shape.rounded_rect
 --end)
 
--- awful.spawn.with_shell("nitrogen --restore")
-awful.spawn.with_shell("wal -R")
-awful.spawn.with_shell("razer-cli -e reactive -a")
-awful.spawn.with_shell("picom --experimental-backends -b")
--- awful.spawn.with_shell("redshift -l 1.3521:103.8198")
-
+-- Autostart
+local autostart = require("autostart")
